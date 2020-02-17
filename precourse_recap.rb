748bf6d2798094c0460a4d05f5e27b4ee8a9c318
Sam <samhum90@gmail.com>
2020-02-17 16:25:45 +0000

    precourse homework base file

---
+++ precourse_recap.rb
@@ -0,0 +1,31 @@
+#Conditionals
+if year == 2020
+  print "This is the current year!!"
+else
+  print "you are in the wrong Timeline"
+
+end
+
+ #Functions
+
+ def year
+   p "year is 2020"
+end
+
+#Array
+Countries = ["America","Britian","France"]
+
+#Hash
+Dog = {"name" => "rover",
+  "weight" => "56Kg",
+  "color" => "Gold"}
+
+#Loop
+  counter = 15
+  target = 3
+
+  while counter >= target
+     print "#{counter}"
+     counter = counter - 1
+
+  end
